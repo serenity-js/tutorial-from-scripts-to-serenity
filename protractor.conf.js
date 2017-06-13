7e57e800189bea16f19c87bfea03bdc56fbf1fb1
jan-molak <jan.molak@gmail.com>
2017-06-13 23:42:23 +0100

    Switched from direct connect to standalone selenium server jar as that seems more stable

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,14 @@
+const
+    glob         = require('glob'),
+    protractor   = require.resolve('protractor'),
+    node_modules = protractor.substring(0, protractor.lastIndexOf('node_modules') + 'node_modules'.length),
+    seleniumJar  = glob.sync(`${node_modules}/protractor/**/selenium-server-standalone-*.jar`).pop();
+
 exports.config = {
 
     baseUrl: 'http://todomvc.com',
+
+    seleniumServerJar: seleniumJar,
 
     // https://github.com/angular/protractor/blob/master/docs/timeouts.md
     allScriptsTimeout: 110000,
