5e25d59e939545b69eae4513aa0a0b1d6009b2c7
jan-molak <jan.molak@gmail.com>
2017-06-21 21:06:27 +0200

    Removed the after step as the default  url that protractor uses prevents us to call window.sessionStorage

--- features/step_definitions/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { protractor } from 'protractor';
-
-export = function hooks() {
-
-    this.After(() => protractor.browser.executeScriptWithDescription([
-        'window.sessionStorage.clear();',
-        'window.localStorage.clear();',
-    ].join('\n'), 'Clean up local storage'));
-};
