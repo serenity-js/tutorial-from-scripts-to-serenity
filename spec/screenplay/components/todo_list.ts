d7b2e99056e2ebc0c92bdf3a7d2dd32f2a6bbff4
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-09-23 22:27:49 +0100

    Updated the css selector since the TodoMVC app has changed. #7

--- spec/screenplay/components/todo_list.ts
+++ spec/screenplay/components/todo_list.ts
@@ -4,7 +4,7 @@ import { by } from 'protractor';
 
 export class TodoList {
     static What_Needs_To_Be_Done = Target.the('"What needs to be done?" input box')
-        .located(by.id('new-todo'));
+        .located(by.css('.new-todo'));
 
     static Items           = Target.the('List of Items').located(by.repeater('todo in todos'));
     static Items_Displayed = Text.ofAll(TodoList.Items);
